366b11ee400d412a328c8528bdc94806d8591cb5
BoletaScociis <99562369+BoletaScociis@users.noreply.github.com>
2022-03-12 13:53:39 +0000

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -12,15 +12,15 @@ enum anne_pro_layers {
 
   };
 
-// Also i made another declaration here, maybe they must be apart from layers.
+// Also I made another declaration here, they must be separate from the layers.
 enum {
-/*I declared three Tap Dance (TD) functions to make the switch from layer to layer*/
+/*I declared three Tap Dance (TD) functions to make the switch from layer to layer */
 /*At the end of the document are the functions and the explanation*/
-  TD_LALT_LS1,
+  TD_TAB_LS1,
   TD_TAB_LS2,
   TD_TAB_LS3,
   TD_TAB_LS4,
-  TD_TAB_LS5,
+  TD_LALT_LS5,
 
 };
 // * -------------------------TAP DANCE------------------------- * 
@@ -30,22 +30,19 @@ enum {
 // For you to know why i chose that name.
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  // Tap once for LALT, twice for Switch to Layer 4
-    [TD_LALT_LS1] = ACTION_TAP_DANCE_LAYER_MOVE (KC_LALT, _FN4_LAYER),
     // Tap once for TAB, twice for Switch to Layer1
-    [TD_TAB_LS2] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _FN1_LAYER),
+    [TD_TAB_LS1] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _FN1_LAYER),
     // Tap once for TAB, twice for Switch to Layer2
-    [TD_TAB_LS3] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _FN2_LAYER),
+    [TD_TAB_LS2] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _FN2_LAYER),
     // Tap once for TAB, twice for Switch to Layer3
-    [TD_TAB_LS4] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _FN3_LAYER),
+    [TD_TAB_LS3] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _FN3_LAYER),
     // Tap once for TAB, twice for Switch to Base Layer
-    [TD_TAB_LS5] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _BASE_LAYER),
- 
+    [TD_TAB_LS4] = ACTION_TAP_DANCE_LAYER_MOVE(KC_TAB, _BASE_LAYER),
+    // Tap once for LALT, twice for Switch to Layer 4
+    [TD_LALT_LS5] = ACTION_TAP_DANCE_LAYER_MOVE (KC_LALT, _FN4_LAYER),
     
-// Althoug self explanatory, each function does a tab when you tap once the TAB key
-// But when you press it twice on an interval of 175 ms it will change layer
-// To what layer it changes depends on what layer you are in, as you can see on the
-// keyboard mapping, if you are on base layer, you use the TD_TAB_LS1 wich switches to _FN1_LAYER
+// Each function except TD_LALT_LSS involves tapping the TAB button twice to switch layers. Tapping once functions as a regular TAB. 
+// The principle is the same for TD_LALT_LSS
 // * ----------------------------------------------------------- *
 
 };
@@ -68,10 +65,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BASE_LAYER] = KEYMAP( /* Base */
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-    TD(TD_TAB_LS2), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+    TD(TD_TAB_LS1), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
     KC_DEL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-    KC_LCTL, KC_LGUI, TD(TD_LALT_LS1), KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LGUI, TD(TD_LALT_LS5), KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
 ),
   /*
   * Layer _FN1_LAYER
@@ -90,7 +87,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   */
  [_FN1_LAYER] = KEYMAP( /*Layer 1 (LSFT depressed)*/ 
     LSFT(KC_ESCAPE), LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), LSFT(KC_MINS), LSFT(KC_EQL), KC_BSPC,
-    TD(TD_TAB_LS3), LSFT(KC_Q), LSFT(KC_W), LSFT(KC_E), LSFT(KC_R), LSFT(KC_T), LSFT(KC_Y), LSFT(KC_U), LSFT(KC_I), LSFT(KC_O), LSFT(KC_P), LSFT(KC_LBRC), LSFT(KC_RBRC), LSFT(KC_BSLS),
+    TD(TD_TAB_LS2), LSFT(KC_Q), LSFT(KC_W), LSFT(KC_E), LSFT(KC_R), LSFT(KC_T), LSFT(KC_Y), LSFT(KC_U), LSFT(KC_I), LSFT(KC_O), LSFT(KC_P), LSFT(KC_LBRC), LSFT(KC_RBRC), LSFT(KC_BSLS),
     KC_DEL, LSFT(KC_A), LSFT(KC_S), LSFT(KC_D), LSFT(KC_F), LSFT(KC_G), LSFT(KC_H), LSFT(KC_J), LSFT(KC_K), LSFT(KC_L), LSFT(KC_SCLN), LSFT(KC_QUOT), KC_ENT,
     KC_LSFT, LSFT(KC_Z), LSFT(KC_X), LSFT(KC_C), LSFT(KC_V), LSFT(KC_B), LSFT(KC_N), LSFT(KC_M), LSFT(KC_COMM), LSFT(KC_DOT), LSFT(KC_SLSH), KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
@@ -113,7 +110,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   */
  [_FN2_LAYER] = KEYMAP( /* AltGr Depressed */
     KC_NO, KC_NO, KC_NO, RALT(KC_F3), KC_NO, KC_NO, KC_NO, KC_NO, RALT(KC_F8), RALT(KC_F9), RALT(KC_F10), RALT(KC_F11), RALT(KC_F12), KC_BSPC,
-    TD(TD_TAB_LS4), RALT(KC_Q), RALT(KC_W), RALT(KC_E), RALT(KC_R), RALT(KC_T), RALT(KC_Y), KC_NO, RALT(KC_I), KC_NO, RALT(KC_P), RALT(KC_LBRC), RALT(KC_RBRC), RALT(KC_BSLS),
+    TD(TD_TAB_LS3), RALT(KC_Q), RALT(KC_W), RALT(KC_E), RALT(KC_R), RALT(KC_T), RALT(KC_Y), KC_NO, RALT(KC_I), KC_NO, RALT(KC_P), RALT(KC_LBRC), RALT(KC_RBRC), RALT(KC_BSLS),
     KC_DEL, KC_NO, RALT(KC_S), RALT(KC_D), RALT(KC_F), RALT(KC_G), RALT(KC_H), RALT(KC_J), RALT(KC_K), KC_NO, KC_NO, KC_NO, KC_ENT,
     LALT(KC_LSFT), RALT(KC_Z), RALT(KC_X), RALT(KC_C), RALT(KC_V), RALT(KC_B), KC_NO, RALT(KC_M), KC_NO, KC_NO, RALT(KC_SLSH), KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
@@ -136,7 +133,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   */
  [_FN3_LAYER] = KEYMAP( /* AltGr and LSFT depressed */
     KC_NO, KC_NO, KC_NO, RALT(LSFT(KC_F3)), KC_NO, KC_NO, KC_NO, RALT(LSFT(KC_F7)), KC_NO, RALT(LSFT(KC_F9)), RALT(LSFT(KC_F10)), RALT(LSFT(KC_F11)), RALT(LSFT(KC_F12)), KC_BSPC,
-    TD(TD_TAB_LS5), RALT(LSFT(KC_Q)), RALT(LSFT(KC_W)), RALT(LSFT(KC_E)), RALT(LSFT(KC_R)), RALT(LSFT(KC_T)), KC_NO, RALT(LSFT(KC_U)), RALT(LSFT(KC_I)), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    TD(TD_TAB_LS4), RALT(LSFT(KC_Q)), RALT(LSFT(KC_W)), RALT(LSFT(KC_E)), RALT(LSFT(KC_R)), RALT(LSFT(KC_T)), KC_NO, RALT(LSFT(KC_U)), RALT(LSFT(KC_I)), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_DEL, KC_NO, RALT(LSFT(KC_S)), RALT(LSFT(KC_D)), KC_NO, RALT(LSFT(KC_G)), RALT(LSFT(KC_H)), RALT(LSFT(KC_J)), KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT,
     LALT(KC_LSFT), RALT(LSFT(KC_Z)), RALT(LSFT(KC_X)), RALT(LSFT(KC_C)), RALT(LSFT(KC_V)), KC_NO, KC_NO, RALT(LSFT(KC_M)), KC_NO, KC_NO, RALT(LSFT(KC_SLSH)), KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
@@ -158,7 +155,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   */
 [_FN4_LAYER] = KEYMAP( /* Bluetooth and LED settings layer */
    KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_PROFILE, KC_AP_LED_PREV_PROFILE, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_AP2_USB, KC_AP2_BT_UNPAIR, KC_NO,
-   TD(TD_TAB_LS5), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
+   TD(TD_TAB_LS1), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_TRNS
@@ -188,17 +185,23 @@ void keyboard_post_init_user(void) {
 
 layer_state_t layer_state_set_user(layer_state_t layer) {
   switch(get_highest_layer(layer)) {
+    case _BASE_LAYER:
+      annepro2LedSetProfile(0);
+      break;
     case _FN1_LAYER:
-      // Set the leds to green
-      annepro2LedSetForegroundColor(0x00, 0xFF, 0x00);
+      annepro2LedSetProfile(1);
       break;
     case _FN2_LAYER:
-      // Set the leds to blue
-      annepro2LedSetForegroundColor(0x00, 0x00, 0xFF);
+      annepro2LedSetProfile(2);
       break;
+    case _FN3_LAYER:
+      annepro2LedSetProfile(3);
+      break; 
+    case _FN4_LAYER:
+      annepro2LedSetProfile(4);
+      break; 
     default:
-      // Reset back to the current profile
-      annepro2LedResetForegroundColor();
+      annepro2LedSetProfile(5);
       break;
   }
   return layer;
